c391d5d74cddefe557a64e567c9f4e9f4c261c75
Titou-Dh <titoudh1@gmail.com>
2024-02-05 15:37:55 +0100

    fixed patients page

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,49 +1,68 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { FaGreaterThan } from 'react-icons/fa';
-import { patients } from '../data/dummy.js';
+import { patients } from '../data/dummy';
 
-function Patients() {
-  return (
-    <div className="p-4 ">
-      <div className="flex items-end gap-2 my-4">
-        <NavLink to="/" key="0">
-          <span className="capitalize text-xl ">dashboard</span>
-        </NavLink>
-        <FaGreaterThan className="text-gray-400 text-xl" />
-        <NavLink to="/patients" key="0">
-          <span className="capitalize text-xl ">Patients</span>
-        </NavLink>
-        <FaGreaterThan className="text-gray-400 text-xl" />
-      </div>
-      <div
-        className="px-12 py-7 rounded-3xl  overflow-scroll bg-white"
-        style={{ height: '80vh' }}
-      >
-        <table className="min-w-full border-gray-300">
-          <tbody>
-            {patients.map((patient) => (
-              <tr
-                key={patient.id}
-                className="p-9"
-              >
-                <td className="py-6 px-4 border-b">{patient.id}</td>
-                <td className="py-6 px-4 border-b">
-                  <NavLink to={`/patients/${patient.id}`} className="text-blue-500">
-                    {patient.first_name}
-                  </NavLink>
-                </td>
-                <td className="py-6 px-4 border-b">{patient.last_name}</td>
-                <td className="py-6 px-4 border-b">{patient.email}</td>
-                <td className="py-6 px-4 border-b">{patient.gender}</td>
-                <td className="py-6 px-4 border-b">{patient.ip_address}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+const Patients = () => {
+  <div className="p-4 ">
+    <div className="flex items-end gap-2 my-4">
+      <NavLink to="/" key="0">
+        <span className="capitalize text-xl ">dashboard</span>
+      </NavLink>
+      <FaGreaterThan className="text-gray-400 text-xl" />
+      <NavLink to="/patients" key="0">
+        <span className="capitalize text-xl ">Patients</span>
+      </NavLink>
+      <FaGreaterThan className="text-gray-400 text-xl" />
     </div>
-  );
-}
+    <div
+      className="bg-white rounded-2xl px-10 py-10 overflow-scroll"
+      style={{ height: '80vh' }}
+    >
+      <table className="min-w-full bg-white  border-gray-300">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 border-b">
+              ID
+            </th>
+            <th className="py-2 px-4 border-b">
+              Firstname
+            </th>
+            <th className="py-2 px-4 border-b">
+              Lastname
+            </th>
+            <th className="py-2 px-4 border-b">
+              Email
+            </th>
+            <th className="py-2 px-4 border-b">
+              Gender
+            </th>
+            <th className="py-2 px-4 border-b">
+              Ip Address
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {patients.map((patient) => (
+            <tr
+              key={patient.id}
+            >
+              <td className="py-4 px-4 border-b">{patient.id}</td>
+              <td className="py-4 px-4 border-b">
+                <Link to={`/patients/${patient.id}`} className="text-blue-500">
+                  {patient.first_name}
+                </Link>
+              </td>
+              <td className="py-4 px-4 border-b">{patient.last_name}</td>
+              <td className="py-4 px-4 border-b">{patient.email}</td>
+              <td className="py-4 px-4 border-b">{patient.gender}</td>
+              <td className="py-4 px-4 border-b">{patient.ip_address}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>;
+};
 
 export default Patients;
