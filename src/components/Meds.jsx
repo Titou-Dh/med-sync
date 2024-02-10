cbb43f9a963f088fd47646e892d614c36677e41a
Titou-Dh <titoudh1@gmail.com>
2024-02-11 00:35:02 +0100

    final touches in Meds jsx

--- src/components/Meds.jsx
+++ src/components/Meds.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Meds() {
   return (
-    <div className=" h-64 w-full px-8 flex flex-col items-center justify-around  bg-white relative rounded-3xl">
-      <h1 className=" ">Meds</h1>
+    <div className=" h-64 w-full px-8 flex flex-col items-center justify-around font-semibold bg-white relative rounded-3xl">
+      <h1 className=" text-left w-full font-bold">Meds</h1>
       <div className="w-11/12 ">
-        <ul className="flex w-full content-around justify-center items-center">
-          <li className="w-1/5">Name</li>
-          <li className="w-1/5">First day</li>
-          <li className="w-1/5">Last day</li>
-          <li className="w-1/5"> Dose</li>
-          <li className="w-1/5">Status</li>
+        <ul className="flex w-full justify-around  items-center">
+          <li className="">Name</li>
+          <li className="">First day</li>
+          <li className="">Last day</li>
+          <li className=""> Dose</li>
+          <li className="">Status</li>
         </ul>
       </div>
       <div className="h-1/2 w-full  rounded-3xl overflow-y-auto">
@@ -18,12 +18,12 @@ function Meds() {
           {[...Array(20).keys()].map((index) => (
             <li key={index} className=" h-10 w-full bg-softBlue">
               <table className="w-full ">
-                <tr>
-                  <td className="w-1/5 text-center border-b-0">Doliprane</td>
-                  <td className="w-1/5 text-center border-b-0">01 Jan 2023</td>
-                  <td className="w-1/5 text-center border-b-0">01 Feb 2023</td>
-                  <td className="w-1/5 text-center border-b-0">3 by day</td>
-                  <td className="w-1/5 text-center border-b-0">Complete</td>
+                <tr className="flex items-center justify-around">
+                  <td className=" text-center border-b-0">Doliprane</td>
+                  <td className=" text-center border-b-0">01 Jan 2023</td>
+                  <td className=" text-center border-b-0">01 Feb 2023</td>
+                  <td className=" text-center border-b-0">3 by day</td>
+                  <td className=" text-center border-b-0">Complete</td>
                 </tr>
               </table>
             </li>
