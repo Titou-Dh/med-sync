b87aec269e97dc5b544ff6d7ff7991afc6bf7c44
Titou-Dh <titoudh1@gmail.com>
2024-02-10 23:15:41 +0100

    optimized the app.js
    by reduced the uneessecary components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, PatientDetail, Patients } from './pages';
+import { Ecommerce, Calendar, ColorPicker, ColorMapping, PatientDetail, Patients } from './pages';
 import './App.css';
 
 import { useStateContext } from './contexts/ContextProvider';
@@ -72,22 +72,22 @@ const App = () => {
                 <Route path="/setting" element={<Calendar />} />
                 <Route path="/patients/:id" element={<PatientDetail />} />
                 {/* pages  */}
-                <Route path="/orders" element={<Orders />} />
-                <Route path="/employees" element={<Employees />} />
-                <Route path="/customers" element={<Customers />} />
+                {/* <Route path="/orders" element={<Orders />} /> */}
+                {/* <Route path="/employees" element={<Employees />} /> */}
+                {/* <Route path="/customers" element={<Customers />} /> */}
                 {/* apps */}
-                <Route path="/kanban" element={<Kanban />} />
-                <Route path="/editor" element={<Editor />} />
+                {/* <Route path="/kanban" element={<Kanban />} /> */}
+                {/* <Route path="/editor" element={<Editor />} /> */}
                 <Route path="/color-picker" element={<ColorPicker />} />
                 {/* charts */}
-                <Route path="/line" element={<Line />} />
-                <Route path="/area" element={<Area />} />
-                <Route path="/bar" element={<Bar />} />
-                <Route path="/pie" element={<Pie />} />
-                <Route path="/financial" element={<Financial />} />
+                {/* <Route path="/line" element={<Line />} /> */}
+                {/* <Route path="/area" element={<Area />} /> */}
+                {/* <Route path="/bar" element={<Bar />} /> */}
+                {/* <Route path="/pie" element={<Pie />} /> */}
+                {/* <Route path="/financial" element={<Financial />} /> */}
                 <Route path="/color-mapping" element={<ColorMapping />} />
-                <Route path="/pyramid" element={<Pyramid />} />
-                <Route path="/stacked" element={<Stacked />} />
+                {/* <Route path="/pyramid" element={<Pyramid />} /> */}
+                {/* <Route path="/stacked" element={<Stacked />} /> */}
               </Routes>
             </div>
             <Footer />
